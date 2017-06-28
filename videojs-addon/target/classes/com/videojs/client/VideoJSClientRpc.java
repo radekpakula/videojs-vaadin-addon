19cbaab8d691a11703618e7056b3b5529e36e15b
Radek <radekpakula@gmail.com>
2017-06-28 10:26:07 +0200

    clean source

--- videojs-addon/target/classes/com/videojs/client/VideoJSClientRpc.java
+++
@@ -1,7 +0,0 @@
-package com.videojs.client;
-
-import com.vaadin.shared.communication.ClientRpc;
-
-public interface VideoJSClientRpc extends ClientRpc {
-
-}
--- videojs-addon/target/classes/com/videojs/client/VideoJSServerRpc.java
+++
@@ -1,7 +0,0 @@
-package com.videojs.client;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface VideoJSServerRpc extends ServerRpc {
-
-}
